ab4fa39dec5baef120aa64c1317f236b8cb87b3d
Felipe <felipe>
2020-09-15 15:29:28 -0300

    Project Created

---
+++ Obligatorio1DA2/LogicInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
